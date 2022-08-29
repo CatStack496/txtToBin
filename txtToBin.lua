c0c05a25d56e363eaa89739c1f9460ea7f570f94
CatStack496 <73456721+CatStack496@users.noreply.github.com>
2022-08-29 16:28:22 -0400

    Add files via upload

--- txtToBin.lua
+++ txtToBin.lua
@@ -1,27 +1,46 @@
-local function int2bin(n)
-    local result = {}
-    while n ~= 0 do
-      if n % 2 == 0 then
-        result[#result+1] = '0'
-      else
-        result[#result+1] = '1'
-      end
-      n = math.floor(n / 2)
-    end
-    return table.concat(result)
-  end
-
-print(int2bin(31))
+local function int2bin(x)
+  ret=""
+	while x~=1 and x~=0 do
+		ret=tostring(x%2)..ret
+		x=math.modf(x/2)
+	end
+	ret=tostring(x)..ret
+	return ret
+end
 
 charMap = {
-    ['0'] = '000',
-    ['1'] = '001',
-    ['2'] = '010',
-    ['3'] = '011',
-    ['4'] = '100',
-    ['5'] = '101',
-    ['6'] = '110',
-    ['7'] = '111'
+space = 0,
+a = 1,
+b = 2,
+c = 3,
+d = 4,
+e = 5,
+f = 6,
+g = 7,
+h = 8,
+i = 9,
+j = 10,
+k = 11,
+l = 12,
+m = 13,
+n = 14,
+o = 15,
+p = 16,
+q = 17,
+r = 18,
+s = 19,
+t = 20,
+u = 21,
+v = 22,
+w = 23,
+x = 24,
+y = 25,
+z = 26,
+db_quote = 27,
+question = 28,
+exclaim = 29,
+comma = 30,
+full_stop = 31   
 }
 
 function file_exists(file)
@@ -42,9 +61,30 @@ function charsFrom(file)
     return chars
 end
   
-  -- tests the functions above
+function getCharMap(char, bits)
+  local rtn = ""
+if char == ' ' then
+  rtn = int2bin(charMap.space)
+elseif char == '"' then
+  rtn = int2bin(charMap.db_quote)
+elseif char == "?" then
+  rtn = int2bin(charMap.question)
+elseif char == "!" then
+  rtn = int2bin(charMap.exclaim)
+elseif char == "," then
+  rtn = int2bin(charMap.comma)
+elseif char == "." then
+  rtn = int2bin(charMap.full_stop)
+else
+  rtn = int2bin(charMap[char])
+end
+for i = #rtn + 1, bits, 1 do
+  rtn = "0" .. rtn
+end
+return rtn
+end
 
 decoded = charsFrom("input.txt")
-for k,v in pairs(decoded) do
-    print(k, v, int2bin(k))
+for k,vk in pairs(decoded) do
+    print(k, vk, getCharMap(vk, 5))
 end
